185edd0f5cb7cc8ba9eda9d67740523827140eb7
LONELY88 <haohsienwong@hotmail.com>
2024-03-04 22:51:57 +0800

    Added Customer Transaction Module
    1. Create Customer Transaction Entity has done
    2. DbContext has created
    3. Pending in CustomerTransaction DTO and WebService

---
+++ aspnet-core/src/SND.SMP.Core/CustomerTransactions/CustomerTransaction.cs
@@ -0,0 +1,23 @@
+﻿using Abp.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SND.SMP.CustomerTransactions
+{
+    public class CustomerTransaction : Entity<long>
+    {
+        public string Wallet { get; set; }
+        public string Customer { get; set; }
+        public string PaymentMode { get; set; }
+        public string Currency { get; set; }
+        public string TransactionType { get; set; }
+        public decimal Amount { get; set; }
+        public string ReferenceNo { get; set; }
+        public string Description { get; set; }
+        public DateTime TransactionDate { get; set; }
+
+    }
+}
