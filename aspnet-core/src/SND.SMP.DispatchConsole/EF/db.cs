50b4e9a21dcab2a7cdf2f44b24d83d448b597efc
coldarse <37390180+coldarse@users.noreply.github.com>
2024-04-12 19:14:57 +0800

    Changed Connection String for new DB

--- aspnet-core/src/SND.SMP.DispatchConsole/EF/db.cs
+++ aspnet-core/src/SND.SMP.DispatchConsole/EF/db.cs
@@ -58,7 +58,7 @@ public partial class db : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseMySql("server=signaturemail.co;port=3306;database=SMPDb;uid=smi_dev;pwd=C56bV{(}Igx<Pqx", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.30-mysql"));
+        => optionsBuilder.UseMySql("server=65.21.224.66;port=3306;database=SMPDb;uid=droot;pwd=snd@1234", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.30-mysql"));
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
