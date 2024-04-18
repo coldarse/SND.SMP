cc9b6710097b5d4f4dfaa2f7c64e9c0c65056729
coldarse <37390180+coldarse@users.noreply.github.com>
2024-04-19 04:36:35 +0800

    Fixed Weight Variance Checking, Added Loading, Back Button and Download Template for PostCheckUpload

--- angular/src/app/post-checks/post-checks.component.ts
+++ angular/src/app/post-checks/post-checks.component.ts
@@ -1,8 +1,12 @@
 import { Component, OnInit } from "@angular/core";
 import { CommonModule } from "@angular/common";
-import { ActivatedRoute } from "@angular/router";
+import { ActivatedRoute, Router } from "@angular/router";
 import { DispatchService } from "@shared/service-proxies/dispatches/dispatch.service";
 import { GetPostCheck } from "@shared/service-proxies/dispatches/model";
+import { HttpErrorResponse } from "@angular/common/http";
+import { BsModalRef, BsModalService } from "ngx-bootstrap/modal";
+import { ErrorModalComponent } from "@shared/components/error-modal/error-modal.component";
+import * as XLSX from "xlsx";
 
 @Component({
   selector: "app-post-checks",
@@ -10,13 +14,12 @@ import { GetPostCheck } from "@shared/service-proxies/dispatches/model";
   styleUrl: "./post-checks.component.css",
 })
 export class PostChecksComponent implements OnInit {
-
   constructor(
     private _Activatedroute: ActivatedRoute,
-    private _dispatchService: DispatchService 
-  ){
-
-  }
+    private _dispatchService: DispatchService,
+    private _modalService: BsModalService,
+    private router: Router
+  ) {}
   bypassValue: string = "";
   fileUpload: any;
 
@@ -34,26 +37,52 @@ export class PostChecksComponent implements OnInit {
   postCheckWeight: number = 380.8;
 
   sub: any;
-  postchecks: GetPostCheck;
+  postchecks: GetPostCheck = undefined;
 
-  bags: any[] = []
+  bags: any[] = [];
 
   ngOnInit(): void {
+    this.postchecks = undefined;
     this.sub = this._Activatedroute.paramMap.subscribe((params) => {
-      this.dispatchNo = params.get('dispatchNo');
-      this._dispatchService.getPostCheck(this.dispatchNo).subscribe((result: any) => {
-        this.postchecks = result.result;
-      });
+      this.dispatchNo = params.get("dispatchNo");
+      this._dispatchService.getPostCheck(this.dispatchNo).subscribe(
+        (result: any) => {
+          this.postchecks = result.result;
+        },
+        (error: HttpErrorResponse) => {
+          this.postchecks = {} as GetPostCheck;
+          //Handle error
+          let cc: BsModalRef;
+          cc = this._modalService.show(ErrorModalComponent, {
+            class: "modal-lg",
+            initialState: {
+              title: "",
+              errorMessage: error.message,
+            },
+          });
+        }
+      );
     });
-      
   }
 
-  calculate(index: number) {
-    let bag = this.bags.at(index);
-    bag.weightVariance = bag.totalWeight - bag.actualWeight;
+  calculate(event: any, index: any) {
+    let bag = this.postchecks.bags.find((x) => x.id === index.id);
+    bag.weightPost = +event.target.value;
+    bag.weightVariance = bag.weightPre - bag.weightPost;
   }
 
-  diff = (a, b) => {
-    return Math.abs(a - b);
-  };
+  backToHome() {
+    this.router.navigate(["/app/home"]);
+  }
+
+  downloadTemplate() {
+
+    let postCheckHeaders:string[] = ["Postal","Dispatch Date","Service","Product Code","Bag No","Country","Weight","Quantity","Seal Number","Dispatch Name"];
+    let postCheckTemplate:string[][] = [postCheckHeaders,[]];
+
+    var wb = XLSX.utils.book_new();
+    var ws = XLSX.utils.aoa_to_sheet(postCheckTemplate);
+    XLSX.utils.book_append_sheet(wb, ws, 'Sheet 1');
+    XLSX.writeFile(wb, `PostCheckUpload.xlsx`);
+  }
 }
