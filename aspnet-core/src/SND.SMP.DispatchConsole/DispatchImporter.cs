0afb2b386a2e6c7ffb0e0d4635dcdec3f69b2fba
coldarse <37390180+coldarse@users.noreply.github.com>
2024-06-20 21:26:48 +0800

    Changed TookInSec to 0 in DispatchImporter

--- aspnet-core/src/SND.SMP.DispatchConsole/DispatchImporter.cs
+++ aspnet-core/src/SND.SMP.DispatchConsole/DispatchImporter.cs
@@ -51,7 +51,7 @@ namespace SND.SMP.DispatchConsole
 
                     newTask.Status = QueueEnumConst.STATUS_RUNNING;
                     newTask.ErrorMsg = null;
-                    newTask.TookInSec = null;
+                    newTask.TookInSec = 0;
 
                     await db.SaveChangesAsync();
                 }
