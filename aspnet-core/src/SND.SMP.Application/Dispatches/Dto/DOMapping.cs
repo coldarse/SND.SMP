781629a80a9d8607c25653b42983c533dc6c4614
coldarse <37390180+coldarse@users.noreply.github.com>
2024-06-26 23:12:03 +0800

    Updates changes for DO according to requirements
    1. All manifest and bag excels will check for displayName for header.
    2. DO change weight to KG
    3. Added DOMapping for required country mapping to origin, destination and service.
    4. BagNo now uses index instead of actual BagNo for DO
    5. Updated ByPass Postcheck to use update range

---
+++ aspnet-core/src/SND.SMP.Application/Dispatches/Dto/DOMapping.cs
@@ -0,0 +1,7 @@
+public class DOMapping
+{
+    public string CountryCode { get; set; }
+    public string Origin { get; set; }
+    public string Destination { get; set; }
+    public string Service { get; set; }
+}
