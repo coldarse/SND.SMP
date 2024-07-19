7bb429d4f0ee048339e08d8f3042393bd22738a7
coldarse <37390180+coldarse@users.noreply.github.com>
2024-07-19 21:10:12 +0800

    Fixed Items null error exception

--- aspnet-core/src/SND.SMP.DispatchConsole/DispatchTrackingUpdater.cs
+++ aspnet-core/src/SND.SMP.DispatchConsole/DispatchTrackingUpdater.cs
@@ -219,23 +219,44 @@ namespace SND.SMP.DispatchConsole
 
         private static Item UpdatedItemTracking(Item item, Stage stage)
         {
-            if (!item.Stage1StatusDesc.Equals(stage.Stage1Desc) || item.Stage1StatusDesc.IsNullOrWhiteSpace() && !stage.Stage1Desc.IsNullOrWhiteSpace()) item.Stage1StatusDesc = stage.Stage1Desc;
-            if (!item.Stage2StatusDesc.Equals(stage.Stage2Desc) || item.Stage2StatusDesc.IsNullOrWhiteSpace() && !stage.Stage2Desc.IsNullOrWhiteSpace()) item.Stage2StatusDesc = stage.Stage2Desc;
-            if (!item.Stage3StatusDesc.Equals(stage.Stage3Desc) || item.Stage3StatusDesc.IsNullOrWhiteSpace() && !stage.Stage3Desc.IsNullOrWhiteSpace()) item.Stage3StatusDesc = stage.Stage3Desc;
-            if (!item.Stage4StatusDesc.Equals(stage.Stage4Desc) || item.Stage4StatusDesc.IsNullOrWhiteSpace() && !stage.Stage4Desc.IsNullOrWhiteSpace()) item.Stage4StatusDesc = stage.Stage4Desc;
-            if (!item.Stage5StatusDesc.Equals(stage.Stage5Desc) || item.Stage5StatusDesc.IsNullOrWhiteSpace() && !stage.Stage5Desc.IsNullOrWhiteSpace()) item.Stage5StatusDesc = stage.Stage5Desc;
-            if (!item.Stage6StatusDesc.Equals(stage.Stage6Desc) || item.Stage6StatusDesc.IsNullOrWhiteSpace() && !stage.Stage6Desc.IsNullOrWhiteSpace()) item.Stage6StatusDesc = stage.Stage6Desc;
-            if (!item.Stage7StatusDesc.Equals(stage.Airport) || item.Stage7StatusDesc.IsNullOrWhiteSpace() && !stage.Airport.IsNullOrWhiteSpace()) item.Stage7StatusDesc = stage.Airport;
-            if (!item.Stage8StatusDesc.Equals(stage.Stage7Desc) || item.Stage8StatusDesc.IsNullOrWhiteSpace() && !stage.Stage7Desc.IsNullOrWhiteSpace()) item.Stage8StatusDesc = stage.Stage7Desc;
+            if(!stage.Stage1Desc.IsNullOrWhiteSpace() && item.Stage1StatusDesc.IsNullOrWhiteSpace() || item.Stage1StatusDesc != stage.Stage1Desc) item.Stage1StatusDesc = stage.Stage1Desc;
+            if(!stage.Stage2Desc.IsNullOrWhiteSpace() && item.Stage2StatusDesc.IsNullOrWhiteSpace() || item.Stage2StatusDesc != stage.Stage2Desc) item.Stage2StatusDesc = stage.Stage2Desc;
+            if(!stage.Stage3Desc.IsNullOrWhiteSpace() && item.Stage3StatusDesc.IsNullOrWhiteSpace() || item.Stage3StatusDesc != stage.Stage3Desc) item.Stage3StatusDesc = stage.Stage3Desc;
+            if(!stage.Stage4Desc.IsNullOrWhiteSpace() && item.Stage4StatusDesc.IsNullOrWhiteSpace() || item.Stage4StatusDesc != stage.Stage4Desc) item.Stage4StatusDesc = stage.Stage4Desc;
+            if(!stage.Stage5Desc.IsNullOrWhiteSpace() && item.Stage5StatusDesc.IsNullOrWhiteSpace() || item.Stage5StatusDesc != stage.Stage5Desc) item.Stage5StatusDesc = stage.Stage5Desc;
+            if(!stage.Stage6Desc.IsNullOrWhiteSpace() && item.Stage6StatusDesc.IsNullOrWhiteSpace() || item.Stage6StatusDesc != stage.Stage6Desc) item.Stage6StatusDesc = stage.Stage6Desc;
+            if(!stage.Airport   .IsNullOrWhiteSpace() && item.Stage7StatusDesc.IsNullOrWhiteSpace() || item.Stage7StatusDesc != stage.Airport   ) item.Stage7StatusDesc = stage.Airport;
+            if(!stage.Stage7Desc.IsNullOrWhiteSpace() && item.Stage8StatusDesc.IsNullOrWhiteSpace() || item.Stage8StatusDesc != stage.Stage7Desc) item.Stage8StatusDesc = stage.Stage7Desc;
 
-            if (!item.DateStage1.Equals(stage.Stage1DateTime) || item.DateStage1 is null && stage.Stage1DateTime is not null) item.DateStage1 = stage.Stage1DateTime;
-            if (!item.DateStage2.Equals(stage.Stage2DateTime) || item.DateStage2 is null && stage.Stage2DateTime is not null) item.DateStage2 = stage.Stage2DateTime;
-            if (!item.DateStage3.Equals(stage.Stage3DateTime) || item.DateStage3 is null && stage.Stage3DateTime is not null) item.DateStage3 = stage.Stage3DateTime;
-            if (!item.DateStage4.Equals(stage.Stage4DateTime) || item.DateStage4 is null && stage.Stage4DateTime is not null) item.DateStage4 = stage.Stage4DateTime;
-            if (!item.DateStage5.Equals(stage.Stage5DateTime) || item.DateStage5 is null && stage.Stage5DateTime is not null) item.DateStage5 = stage.Stage5DateTime;
-            if (!item.DateStage6.Equals(stage.Stage6DateTime) || item.DateStage6 is null && stage.Stage6DateTime is not null) item.DateStage6 = stage.Stage6DateTime;
-            if (!item.DateStage7.Equals(stage.AirportDateTime) || item.DateStage7 is null && stage.AirportDateTime is not null) item.DateStage7 = stage.AirportDateTime;
-            if (!item.DateStage8.Equals(stage.Stage7DateTime) || item.DateStage8 is null && stage.Stage7DateTime is not null) item.DateStage8 = stage.Stage7DateTime;
+            if(stage.Stage1DateTime  is not null && item.DateStage1 is null || item.DateStage1 != stage.Stage1DateTime)  item.DateStage1 = stage.Stage1DateTime;
+            if(stage.Stage2DateTime  is not null && item.DateStage2 is null || item.DateStage2 != stage.Stage2DateTime)  item.DateStage2 = stage.Stage2DateTime;
+            if(stage.Stage3DateTime  is not null && item.DateStage3 is null || item.DateStage3 != stage.Stage3DateTime)  item.DateStage3 = stage.Stage3DateTime;
+            if(stage.Stage4DateTime  is not null && item.DateStage4 is null || item.DateStage4 != stage.Stage4DateTime)  item.DateStage4 = stage.Stage4DateTime;
+            if(stage.Stage5DateTime  is not null && item.DateStage5 is null || item.DateStage5 != stage.Stage5DateTime)  item.DateStage5 = stage.Stage5DateTime;
+            if(stage.Stage6DateTime  is not null && item.DateStage6 is null || item.DateStage6 != stage.Stage6DateTime)  item.DateStage6 = stage.Stage6DateTime;
+            if(stage.AirportDateTime is not null && item.DateStage7 is null || item.DateStage7 != stage.AirportDateTime) item.DateStage7 = stage.AirportDateTime;
+            if(stage.Stage7DateTime  is not null && item.DateStage8 is null || item.DateStage8 != stage.Stage7DateTime)  item.DateStage8 = stage.Stage7DateTime;
+
+
+
+
+            // if (!item.Stage1StatusDesc.Equals(stage.Stage1Desc) || item.Stage1StatusDesc.IsNullOrWhiteSpace() && !stage.Stage1Desc.IsNullOrWhiteSpace()) item.Stage1StatusDesc = stage.Stage1Desc;
+            // if (!item.Stage2StatusDesc.Equals(stage.Stage2Desc) || item.Stage2StatusDesc.IsNullOrWhiteSpace() && !stage.Stage2Desc.IsNullOrWhiteSpace()) item.Stage2StatusDesc = stage.Stage2Desc;
+            // if (!item.Stage3StatusDesc.Equals(stage.Stage3Desc) || item.Stage3StatusDesc.IsNullOrWhiteSpace() && !stage.Stage3Desc.IsNullOrWhiteSpace()) item.Stage3StatusDesc = stage.Stage3Desc;
+            // if (!item.Stage4StatusDesc.Equals(stage.Stage4Desc) || item.Stage4StatusDesc.IsNullOrWhiteSpace() && !stage.Stage4Desc.IsNullOrWhiteSpace()) item.Stage4StatusDesc = stage.Stage4Desc;
+            // if (!item.Stage5StatusDesc.Equals(stage.Stage5Desc) || item.Stage5StatusDesc.IsNullOrWhiteSpace() && !stage.Stage5Desc.IsNullOrWhiteSpace()) item.Stage5StatusDesc = stage.Stage5Desc;
+            // if (!item.Stage6StatusDesc.Equals(stage.Stage6Desc) || item.Stage6StatusDesc.IsNullOrWhiteSpace() && !stage.Stage6Desc.IsNullOrWhiteSpace()) item.Stage6StatusDesc = stage.Stage6Desc;
+            // if (!item.Stage7StatusDesc.Equals(stage.Airport   ) || item.Stage7StatusDesc.IsNullOrWhiteSpace() && !stage.Airport   .IsNullOrWhiteSpace()) item.Stage7StatusDesc = stage.Airport;
+            // if (!item.Stage8StatusDesc.Equals(stage.Stage7Desc) || item.Stage8StatusDesc.IsNullOrWhiteSpace() && !stage.Stage7Desc.IsNullOrWhiteSpace()) item.Stage8StatusDesc = stage.Stage7Desc;
+
+            // if (!item.DateStage1.Equals(stage.Stage1DateTime) || item.DateStage1 is null && stage.Stage1DateTime is not null)   item.DateStage1 = stage.Stage1DateTime;
+            // if (!item.DateStage2.Equals(stage.Stage2DateTime) || item.DateStage2 is null && stage.Stage2DateTime is not null)   item.DateStage2 = stage.Stage2DateTime;
+            // if (!item.DateStage3.Equals(stage.Stage3DateTime) || item.DateStage3 is null && stage.Stage3DateTime is not null)   item.DateStage3 = stage.Stage3DateTime;
+            // if (!item.DateStage4.Equals(stage.Stage4DateTime) || item.DateStage4 is null && stage.Stage4DateTime is not null)   item.DateStage4 = stage.Stage4DateTime;
+            // if (!item.DateStage5.Equals(stage.Stage5DateTime) || item.DateStage5 is null && stage.Stage5DateTime is not null)   item.DateStage5 = stage.Stage5DateTime;
+            // if (!item.DateStage6.Equals(stage.Stage6DateTime) || item.DateStage6 is null && stage.Stage6DateTime is not null)   item.DateStage6 = stage.Stage6DateTime;
+            // if (!item.DateStage7.Equals(stage.AirportDateTime)|| item.DateStage7 is null && stage.AirportDateTime is not null)  item.DateStage7 = stage.AirportDateTime;
+            // if (!item.DateStage8.Equals(stage.Stage7DateTime) || item.DateStage8 is null && stage.Stage7DateTime is not null)   item.DateStage8 = stage.Stage7DateTime;
 
             return item;
         }
