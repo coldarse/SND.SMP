9f5aa7dbcbbbea3426932431f53fd871a451d0b6
coldarse <37390180+coldarse@users.noreply.github.com>
2024-07-24 12:30:58 +0800

    Added ItemDetails File

---
+++ aspnet-core/src/SND.SMP.Application/Items/Dto/ItemDetails.cs
@@ -0,0 +1,20 @@
+public class ItemDetails
+{
+    public string TrackingNo { get; set; }
+    public string DispatchNo { get; set; }
+    public string BagNo { get; set; }
+    public string DispatchDate { get; set; }
+    public string Postal { get; set; }
+    public string Service { get; set; }
+    public string Product { get; set; }
+    public string Country { get; set; }
+    public decimal Weight { get; set; }
+    public decimal Value { get; set; }
+    public string Description { get; set; }
+    public string ReferenceNo { get; set; }
+    public string Recipient { get; set; }
+    public string ContactNo { get; set; }
+    public string Email { get; set; }
+    public string Address { get; set; }
+    public int Status { get; set; }
+}
