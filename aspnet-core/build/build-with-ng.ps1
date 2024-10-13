5aaf7be39d5fb614c6bca1198f6d16d3e329a8c8
coldarse <37390180+coldarse@users.noreply.github.com>
2024-10-13 23:20:12 +0800

    Deployed DE Changes for Pre Register ItemId

--- aspnet-core/build/build-with-ng.ps1
+++ aspnet-core/build/build-with-ng.ps1
@@ -2,7 +2,7 @@ echo " Welcome to docker build"
 echo ""
 echo ""
 
-$ABP_HOST="dannychen98/smp_api:v1.2.33"
+$ABP_HOST="dannychen98/smp_api:v1.2.35"
 $ABP_HOST_DOCKERFILE_PATH="src/SND.SMP.Web.Host/Dockerfile"
 $SMP_DISPATCH_DOCKERFILE_PATH="src/SND.SMP.DispatchConsole/Dockerfile"
 $ABP_NG="dannychen98/smp_ms:v1.2.25"
