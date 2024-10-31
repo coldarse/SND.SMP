21cc2cfe3f6ad5daea4768d554d65a10ff0349ab
coldarse <37390180+coldarse@users.noreply.github.com>
2024-10-31 19:02:19 +0800

    Added Docker Compose Configurations for newly added Background workers

--- aspnet-core/src/SND.SMP.DispatchConsole/Program.cs
+++ aspnet-core/src/SND.SMP.DispatchConsole/Program.cs
@@ -18,6 +18,7 @@ public class Program
         //builder.Services.AddHostedService<WorkerDispatchImport>();
         //builder.Services.AddHostedService<WorkerDispatchValidate>();
         //builder.Services.AddHostedService<WorkerItemTrackingGenerate>();
+        //builder.Services.AddHostedService<WorkerItemTrackingNoUpdate>();
         //builder.Services.AddHostedService<WorkerDispatchTrackingUpdate>();
 
         //builder.Services.AddHostedService<WorkerRateWeightBreak>();
