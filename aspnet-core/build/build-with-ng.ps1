471854ef75942467810cb0207996ebf18b30f0b4
coldarse <37390180+coldarse@users.noreply.github.com>
2024-11-25 10:26:23 +0800

    Added ItemTrakingUpdater Worker

--- aspnet-core/build/build-with-ng.ps1
+++ aspnet-core/build/build-with-ng.ps1
@@ -7,14 +7,15 @@ $ABP_HOST_DOCKERFILE_PATH="src/SND.SMP.Web.Host/Dockerfile"
 $SMP_DISPATCH_DOCKERFILE_PATH="src/SND.SMP.DispatchConsole/Dockerfile"
 $SMP_DISPATCH_1_DOCKERFILE_PATH="src/SND.SMP.DispatchImporter/Dockerfile"
 $SMP_DISPATCH_2_DOCKERFILE_PATH="src/SND.SMP.DispatchValidator/Dockerfile"
-$SMP_DISPATCH_3_DOCKERFILE_PATH="src/SND.SMP.DispatchTrackingUpdater/Dockerfile"
-$SMP_DISPATCH_4_DOCKERFILE_PATH="src/SND.SMP.ItemTrackingGenerator/Dockerfile"
+$SMP_DISPATCH_3_DOCKERFILE_PATH="src/SND.SMP.ItemTrackingGenerator/Dockerfile"
+$SMP_DISPATCH_4_DOCKERFILE_PATH="src/SND.SMP.ItemTrackingUpdater/Dockerfile"
 $ABP_NG="dannychen98/tfs_ms:v1.1.18"
 # $SMP_DISPATCH="dannychen98/tfs_dispatch_console:v1.1.7"
 $SMP_DISPATCH_1="dannychen98/tfs_dispatch_importer:v1.0.3"
 $SMP_DISPATCH_2="dannychen98/tfs_dispatch_validator:v1.0.2"
-$SMP_DISPATCH_3="dannychen98/tfs_dispatch_tracking_updater:v1.0.2"
-$SMP_DISPATCH_4="dannychen98/tfs_item_tracking_generator:v1.0.2"
+$SMP_DISPATCH_3="dannychen98/tfs_item_tracking_generator:v1.0.2"
+$SMP_DISPATCH_4="dannychen98/tfs_item_tracking_updater:v1.0.1"
+
 # cd ..
 # echo " Building docker image $SMP_DISPATCH..."
 # docker build -t $SMP_DISPATCH -f $SMP_DISPATCH_DOCKERFILE_PATH .
@@ -22,12 +23,12 @@ $SMP_DISPATCH_4="dannychen98/tfs_item_tracking_generator:v1.0.2"
 # echo ""
 # echo ""
 
-cd ..
-echo " Building docker image $SMP_DISPATCH_1..."
-docker build -t $SMP_DISPATCH_1 -f $SMP_DISPATCH_1_DOCKERFILE_PATH .
-echo " Done. -- Building docker image $SMP_DISPATCH_1..."
-echo ""
-echo ""
+# cd ..
+# echo " Building docker image $SMP_DISPATCH_1..."
+# docker build -t $SMP_DISPATCH_1 -f $SMP_DISPATCH_1_DOCKERFILE_PATH .
+# echo " Done. -- Building docker image $SMP_DISPATCH_1..."
+# echo ""
+# echo ""
 
 # cd ..
 # echo " Building docker image $SMP_DISPATCH_2..."
@@ -43,12 +44,12 @@ echo ""
 # echo ""
 # echo ""
 
-# cd ..
-# echo " Building docker image $SMP_DISPATCH_4..."
-# docker build -t $SMP_DISPATCH_4 -f $SMP_DISPATCH_4_DOCKERFILE_PATH .
-# echo " Done. -- Building docker image $SMP_DISPATCH_4..."
-# echo ""
-# echo ""
+cd ..
+echo " Building docker image $SMP_DISPATCH_4..."
+docker build -t $SMP_DISPATCH_4 -f $SMP_DISPATCH_4_DOCKERFILE_PATH .
+echo " Done. -- Building docker image $SMP_DISPATCH_4..."
+echo ""
+echo ""
 
 # cd ..
 # echo " Building docker image $ABP_HOST..."
