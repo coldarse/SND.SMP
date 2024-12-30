c28ec12992dee718746332eae1135c5f3b2d2763
coldarse <37390180+coldarse@users.noreply.github.com>
2024-12-30 18:16:07 +0800

    deployed request response ui

--- aspnet-core/build/build-with-ng.ps1
+++ aspnet-core/build/build-with-ng.ps1
@@ -2,14 +2,14 @@ echo " Welcome to docker build"
 echo ""
 echo ""
 
-$ABP_HOST="dannychen98/tfs_api:v1.1.28"
+$ABP_HOST="dannychen98/tfs_api:v1.1.29"
 $ABP_HOST_DOCKERFILE_PATH="src/SND.SMP.Web.Host/Dockerfile"
 $SMP_DISPATCH_DOCKERFILE_PATH="src/SND.SMP.DispatchConsole/Dockerfile"
 $SMP_DISPATCH_1_DOCKERFILE_PATH="src/SND.SMP.DispatchImporter/Dockerfile"
 $SMP_DISPATCH_2_DOCKERFILE_PATH="src/SND.SMP.DispatchValidator/Dockerfile"
 $SMP_DISPATCH_3_DOCKERFILE_PATH="src/SND.SMP.ItemTrackingGenerator/Dockerfile"
 $SMP_DISPATCH_4_DOCKERFILE_PATH="src/SND.SMP.ItemTrackingUpdater/Dockerfile"
-$ABP_NG="dannychen98/tfs_ms:v1.1.18"
+$ABP_NG="dannychen98/tfs_ms:v1.1.19"
 # $SMP_DISPATCH="dannychen98/tfs_dispatch_console:v1.1.7"
 $SMP_DISPATCH_1="dannychen98/tfs_dispatch_importer:v1.0.5"
 $SMP_DISPATCH_2="dannychen98/tfs_dispatch_validator:v1.0.6"
@@ -51,20 +51,20 @@ $SMP_DISPATCH_4="dannychen98/tfs_item_tracking_updater:v1.0.1"
 # echo ""
 # echo ""
 
-cd ..
-echo " Building docker image $ABP_HOST..."
-docker build -t $ABP_HOST -f $ABP_HOST_DOCKERFILE_PATH .
-echo " Done. -- Building docker image $ABP_HOST..."
-echo ""
-echo ""
+# cd ..
+# echo " Building docker image $ABP_HOST..."
+# docker build -t $ABP_HOST -f $ABP_HOST_DOCKERFILE_PATH .
+# echo " Done. -- Building docker image $ABP_HOST..."
+# echo ""
+# echo ""
 
-# cd ..
-# cd ..
-# cd angular/
-# echo " Building docker image $ABP_NG..."
-# docker build -t $ABP_NG -f Dockerfile .
-# echo " Done. -- Building docker image $ABP_NG..."
-# echo ""
-# echo ""
+cd ..
+cd ..
+cd angular/
+echo " Building docker image $ABP_NG..."
+docker build -t $ABP_NG -f Dockerfile .
+echo " Done. -- Building docker image $ABP_NG..."
+echo ""
+echo ""
 
 
